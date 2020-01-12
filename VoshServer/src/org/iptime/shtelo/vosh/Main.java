2eb710e12fa1896d680f31b7e91b97b539921910
junhg0211 <junhg0211@gmail.com>
2020-01-12 19:18:29 +0900

    Project Divided into two
    The server and the client.

---
+++ VoshServer/src/org/iptime/shtelo/vosh/Main.java
@@ -0,0 +1,9 @@
+package org.iptime.shtelo.vosh;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+}
--- src/org/iptime/shtelo/vosh/Main.java
+++
@@ -1,7 +0,0 @@
-package org.iptime.shtelo.vosh;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
